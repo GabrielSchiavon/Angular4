8a5cde1d08e3f67910402f7fb759b112f7e7b911
Gabriel Schiavon <gabriel_schiavon94@hotmail.com>
2017-09-05 23:16:24 -0300

    removendo pasta com exercicio errado

--- exeServicos/src/app/membro.model.ts
+++
@@ -1,9 +0,0 @@
-export class Membro {
-    total: number;
-
-    constructor(public nome: string, public prod: number, public pontos: number, 
-        public bug: number, public cleanCode: number, public auxDev: number) {
-        
-            this.total = 0;
-    }
-}
