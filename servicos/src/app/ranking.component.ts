0048435a66fc0351dc1d07ed809183504c626c13
Gabriel Schiavon <gabriel_schiavon94@hotmail.com>
2017-09-11 18:06:35 -0300

    Ordenando a equipe por maior total e limpando os campos após inserção

--- servicos/src/app/ranking.component.ts
+++ servicos/src/app/ranking.component.ts
@@ -20,6 +20,7 @@ export class RankingComponent {
   membro: Membro;
   equipe: Membro[];
 
+
   constructor(private rankingService: RankingService) {
     this.nome = "";
     this.produtividade = 0;
@@ -31,9 +32,28 @@ export class RankingComponent {
     this.equipe = [];
   }
 
+  ordenaLista(equipe: Membro[]) {
+    equipe.sort((total1, total2) => {
+      if (total1.total < total2.total) return 1;
+      else return 0;
+    });
+  }
+
+  limpaCampos() {
+    this.nome = "";
+    this.produtividade = 0;
+    this.pontos = 0;
+    this.bug = 0;
+    this.cleanCode = 0;
+    this.auxDev = 0;
+    this.total = 0;
+  }
+
   inserirMembro() {
     this.membro = new Membro(this.nome, this.produtividade, this.pontos, this.bug, this.cleanCode, this.auxDev);
     this.membro.total = this.rankingService.calculoTotal(this.membro);
     this.equipe.push(this.membro);
+    this.ordenaLista(this.equipe);
+    this.limpaCampos();
   }
 }
